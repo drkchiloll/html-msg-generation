1ebccfce7d15a1a714e7a469ef73916b989d1e1d
drkchiloll <samwomack_@me.com>
2016-02-11 06:59:28 -0600

    Currently Handles Spark LIB Events (returned lists of room messages and room memberships), and reduction of Array of Arrays into a Single Array where needed

---
+++ helpers.js
@@ -0,0 +1,25 @@
+var Promise = require('bluebird');
+
+exports.handleSprkList = (Spark, item) => {
+  var tempArr = [];
+  return new Promise((resolve, reject) => {
+    Spark.listItemEvt({
+      item: item,
+      roomId: roomid,
+      max: 100
+    }).on(item, resp => {
+      tempArr.push(resp);
+    }).on(`${item}-end`, resp => {
+      tempArr.push(resp);
+      return resolve(tempArr);
+    });
+  });
+};
+
+exports.handleArrArr = (arrArr) => {
+  // Reduce the Array of Arrays to a Single Array
+  return arrArr.reduce((array, arr) => {
+    array = array.concat(arr);
+    return array;
+  });
+};
