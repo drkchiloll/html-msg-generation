8edb9991785aa37ebcefa9e3147278d48f622c2b
drkchiloll <samwomack_@me.com>
2016-02-11 11:34:36 -0600

    refactor: moved a lot of code to helpers.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var Promise = require('bluebird'),
+var utils = require('./helpers');
     config = require('./config'),
     Spark = require('csco-spark')({
       uri:'https://api.ciscospark.com/v1',
@@ -6,43 +6,23 @@ var Promise = require('bluebird'),
     }),
     roomid = config.room;
 
-var handleSprkList = (item) => {
-  var tempArr = [];
-  return new Promise((resolve, reject) => {
-    Spark.listItemEvt({
-      item: item,
-      roomId: roomid,
-      max: 100
-    }).on(item, resp => {
-      tempArr.push(resp);
-    }).on(`${item}-end`, resp => {
-      tempArr.push(resp);
-      return resolve(tempArr);
-    })
-  })
-};
-
-var handleArrArr = (arrArr) => {
-  // Reduce the Array of Arrays to a Single Array
-  return arrArr.reduce((array, arr) => {
-    array = array.concat(arr);
-    return array;
-  });
-};
-
 var sparkMsgs = [],
     roomMembers = [];
 
-handleSprkList('messages').then((arrarr) => {
+// utils.contactPanel(null);
+
+utils.handleSprkList(Spark, 'messages').then((arrarr) => {
   //Handle Array of Arrays
-  sparkMsgs = handleArrArr(arrarr);
+  sparkMsgs = utils.handleArrArr(arrarr);
   return;
 }).then(() => {
-   return handleSprkList('memberships').then((arrarr) => {
-     roomMembers = handleArrArr(arrarr);
+   return utils.handleSprkList(Spark, 'memberships').then((arrarr) => {
+     roomMembers = utils.handleArrArr(arrarr);
      return;
    })
 }).then(() => {
-  console.log(sparkMsgs);
-  console.log(roomMembers);
+  // console.log(sparkMsgs);
+  roomMembers = utils.handleEmails(roomMembers);
+  utils.contactPanel(roomMembers);
+
 });
