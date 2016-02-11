f89add642ecdb5fe7116eeda7d0ac524ef77b180
drkchiloll <samwomack_@me.com>
2016-02-11 06:58:13 -0600

    added helper functions to handle duplicated efforts

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
-var config = require('./config');
-var Spark = require('csco-spark')({
-  uri:'https://api.ciscospark.com/v1',
-  token: config.token
+var Promise = require('bluebird'),
+    config = require('./config'),
+    Spark = require('csco-spark')({
+      uri:'https://api.ciscospark.com/v1',
+      token: config.token
+    }),
+    roomid = config.room;
+
+var handleSprkList = (item) => {
+  var tempArr = [];
+  return new Promise((resolve, reject) => {
+    Spark.listItemEvt({
+      item: item,
+      roomId: roomid,
+      max: 100
+    }).on(item, resp => {
+      tempArr.push(resp);
+    }).on(`${item}-end`, resp => {
+      tempArr.push(resp);
+      return resolve(tempArr);
+    })
+  })
+};
+
+var handleArrArr = (arrArr) => {
+  // Reduce the Array of Arrays to a Single Array
+  return arrArr.reduce((array, arr) => {
+    array = array.concat(arr);
+    return array;
+  });
+};
+
+var sparkMsgs = [],
+    roomMembers = [];
+
+handleSprkList('messages').then((arrarr) => {
+  //Handle Array of Arrays
+  sparkMsgs = handleArrArr(arrarr);
+  return;
+}).then(() => {
+   return handleSprkList('memberships').then((arrarr) => {
+     roomMembers = handleArrArr(arrarr);
+     return;
+   })
+}).then(() => {
+  console.log(sparkMsgs);
+  console.log(roomMembers);
 });
-var roomid = config.room;
-
-var sparkMsgs = [];
-var finalArray = [];
-Spark.listItemEvt({
-  item: 'messages',
-  roomId: roomid,
-  max: 50
-}).on('messages', resp => {
-  sparkMsgs.push(resp);
-}).on('end', resp => {
-  sparkMsgs.push(resp);
-
-  finalArray = sparkMsgs.reduce((arr, sparkMsgArr) => {
-    arr = arr.concat(sparkMsgArr);
-    return arr;
-  }, finalArray);
-
-  console.log(finalArray);
-})
