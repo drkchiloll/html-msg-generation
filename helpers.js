28b8adb5523f4ea934cdfbd28566792ee052623a
drkchiloll <samwomack_@me.com>
2016-02-11 13:47:05 -0600

    Added Moment as a dependency to properly format Dates in the Messages Panel Now using html5-to-pdf to create PDF..you can optionally include paths to CSS (such as bootstrap) Added additional param to contactPanel (roomTitle) and now returning the html as a string Completed the msgPanel and html2Pdf methods

--- helpers.js
+++ helpers.js
@@ -1,6 +1,8 @@
 var Promise = require('bluebird'),
+    moment = require('moment'),
     cheerio = require('cheerio'),
-    fs = require('fs');
+    fs = require('fs'),
+    pdf = require('html5-to-pdf');
 
 exports.handleSprkList = (Spark, item) => {
   var tempArr = [];
@@ -33,12 +35,37 @@ exports.handleEmails = (members) => members
     return arr;
   },[]);
 
-exports.contactPanel = (contacts) => {
+exports.contactPanel = (roomTitle, contacts) => {
   var $ = cheerio.load(fs.readFileSync('index.html'));
-  $('h3#title').text('Generic Room');
+  $('h3#title').text(roomTitle);
   var tdata = $('tbody#tblbody');
   contacts.forEach(contact => {
     tdata.append(`<tr><td>${contact}</td></tr>`);
   });
-  console.log($.html());
+  return $.html();
+};
+
+exports.msgPanel = (html, messages) => {
+  var dtFormat = 'l LT',
+      $ = cheerio.load(html),
+      msgBody = $('div#msgbody');
+  messages.forEach(msg => {
+    if(msg.text) {
+      msgDt = new Date(msg.created);
+      msgBody.append(
+        `<p><strong>${msg.personEmail}</strong> ${moment(msgDt).format(dtFormat)}`+
+        `<p>${(msg.text).includes('\n') ? msg.text.replace(/\n/gi, '<br/>') : msg.text}<p>`+
+        `<hr>`
+      );
+    }
+  });
+  return $.html();
+};
+
+exports.htmlToPdf = (title, html) => {
+  pdf({
+    cssPath: './node_modules/bootstrap/dist/css/bootstrap.css'
+  }).from.string(html).to('./${title}.pdf', () => {
+    console.log('finished');
+  })
 };
